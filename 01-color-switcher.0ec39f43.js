913cc73c1a29c36a9bec2adbe1440bbb77ab6ddb
SerJ1327 <103960630+SerJ1327@users.noreply.github.com>
2023-05-22 20:27:30 +0000

    Deploying to gh-pages from @ SerJ1327/goit-js-hw-09@3d120bb3c945a3e738588bc620106c4ed372a2ab 🚀

---
+++ 01-color-switcher.0ec39f43.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]"),d=document.querySelector("body");a.disabled=!0,e.addEventListener("click",(function(n){t=setInterval((function(){d.style="background-color: ".concat("#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)))}),1e3),e.disabled=!0,a.disabled=!1})),a.addEventListener("click",(function(d){clearInterval(t),e.disabled=!1,a.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.0ec39f43.js.map
